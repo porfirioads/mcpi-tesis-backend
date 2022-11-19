9b631fae34dfe9136eb824c83b5091b6fc8ef393
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2022-11-19 14:44:55 -0600

    feat: change path of cleaned datasets

--- app/routers/dataset_router.py
+++ app/routers/dataset_router.py
@@ -13,23 +13,23 @@ cleaning_service = CleaningService()
 
 @router.get('/', response_model=List[FileUpload])
 def get_dataset_list():
-    logger.debug('get_dataset_list')
+    logger.debug('get_dataset_list()')
     return dataset_service.get_datasets_list()
 
 
 @router.post('/upload', response_model=FileUpload)
 def upload_dataset(file: UploadFile = File()):
-    logger.debug('upload_dataset')
+    logger.debug('upload_dataset()')
     return dataset_service.upload_dataset(file)
 
 
 @router.get('/download')
 def download_dataset(file_path: str):
-    logger.debug('download_dataset')
+    logger.debug('download_dataset()')
     return dataset_service.download_dataset(file_path)
 
 
 @router.get('/clean', response_model=FileUpload)
 def clean_dataset(file_path: str):
-    logger.debug('clean_dataset')
+    logger.debug('clean_dataset()')
     return cleaning_service.clean(file_path)
